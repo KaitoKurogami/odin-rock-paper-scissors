8c3335f55932ae0bc37640488e848bac99b8e4a7
KaitoKurogami <andresmgr@gmail.com>
2024-09-20 12:28:40 -0300

    add the method to get the player's choice

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let num = Math.random()*3;
     if ((0 <= num) && ( num < 1)){
@@ -9,6 +12,21 @@ function getComputerChoice() {
     }
 }
 
-for (let i = 1 ;i < 10; i++){
-    console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let choice = prompt(`write your choice:
+        1: rock
+        2: paper
+        3: scissors`);
+    if ((choice === "1") || (choice === "rock")){
+        return "rock"
+    }else if((choice === "2") || (choice === "paper")){
+        return "paper"
+    }else if((choice === "3") || (choice === "scissors")){
+        return "scissors"
+    }else{
+        console.log("pick one of the 3 options, word or number")
+    }
 }
+
+getHumanChoice();
